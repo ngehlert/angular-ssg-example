fdf1b96dcd1ec2b92c2a1ef7183c6b31f7082877
Nicolas Gehlert <nicolas.gehlert@active-agent.com>
2025-04-24 12:36:34 +0200

    finished example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {PageOneComponent} from './page-one.component';
+import {PageTwoComponent} from './page-two.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'page-one',
+    component: PageOneComponent,
+  },
+  {
+    path: 'page-two/:id',
+    component: PageTwoComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'page-one',
+    pathMatch: 'full',
+  }
+];
